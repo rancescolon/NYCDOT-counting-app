50fafc8509313285e48e7f2e4c964172e57200e6
rancescolon <109361303+rancescolon@users.noreply.github.com>
2026-08-07 11:28:59 -0400

    img fix

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// lib/utils.ts (or inside your config/constants file)
+export const getAssetPath = (path: string) => {
+  const basePath = process.env.NODE_ENV === 'production' ? '/NYCDOT-counting-app' : '';
+  return `${basePath}${path}`;
+};
