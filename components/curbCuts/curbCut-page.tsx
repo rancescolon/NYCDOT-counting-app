892fca4fbd2c4328761fe443e10285acaf4d1e0d
rancescolon <109361303+rancescolon@users.noreply.github.com>
2026-08-20 11:10:50 -0400

    Update curbCut-page.tsx
    removed random animation

--- components/curbCuts/curbCut-page.tsx
+++ components/curbCuts/curbCut-page.tsx
@@ -396,7 +396,7 @@ export default function CurbCutPage() {
   if (!isDataLoaded)
     return (
         <div className="h-screen w-screen bg-gradient-to-br flex items-center justify-center">
-          <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600"></div>
+          {/*<div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600"></div>*/}
         </div>
     )
 
